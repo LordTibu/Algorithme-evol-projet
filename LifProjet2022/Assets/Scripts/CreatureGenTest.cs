35c24d7e743cb562b2c448b7dfccb9236537340d
NicolasP <p1926177@forge.univ-lyon1.fr>
2022-11-17 10:17:43 +0100

    la licuadora de creaturas

--- LifProjet2022/Assets/Scripts/CreatureGenTest.cs
+++ LifProjet2022/Assets/Scripts/CreatureGenTest.cs
@@ -6,42 +6,88 @@ using UnityEngine.Scripting;
 public class CreatureGenTest : MonoBehaviour
 {
     public GameObject creaturePrefab;
-    public GameObject torsoPrefab;
-    public GameObject limbPrefab;
+    public GameObject [] torsoPrefab;
+    public GameObject [] limbPrefab;
     GameObject creature, currentTorso, newTorso, currentLimb, newLimb;
+    Vector3 [] tabpos;
     CreatureDNA adn;
-    int randX = 0, randY = 0, randZ = 0;
     void Start()
     {   
         //On cree une creature (empty object avec script qui contient des info)
-        creature = Object.Instantiate(creaturePrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        creature = Object.Instantiate(creaturePrefab, transform.position, Quaternion.identity);
         //On recupéré les données (qui sont crées aléatoirement en appelant la function .create() )
         adn = creature.GetComponent<CreatureDNA>();
         adn.create();
+        int randIndexTorso = Random.Range(0, torsoPrefab.Length);
+        int randIndexLimb = Random.Range(0, limbPrefab.Length);
+
+
         //On crée un torso; fils de creature
-        currentTorso = Object.Instantiate(torsoPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity, creature.transform);
+        currentTorso = Object.Instantiate(torsoPrefab[randIndexTorso], transform.position, Quaternion.identity, creature.transform);
         //On ajoute les "bras" ou Limbs avec des rotations, positions et dimensions aleatoires (toujours en contact avec torso)
-        for(int i = 0; i < adn.limbNumb; i++){
+
+        
+        Debug.Log("added a limb to main torso right");
+        newLimb = Object.Instantiate(limbPrefab[0], transform.position, Quaternion.identity, currentTorso.transform);
+        ConfigurableJoint fJoint = newLimb.GetComponent<ConfigurableJoint>();
+        fJoint.anchor = new Vector3(0.5f,0,0);
+        int rand = Random.Range(0,2);
+        if (rand == 0)
+        {
+            fJoint.angularYMotion= ConfigurableJointMotion.Free;        
+        }
+
+        rand = Random.Range(0,2);
+        if (rand == 0)
+        {
+            fJoint.angularZMotion= ConfigurableJointMotion.Free;       
+        }
+
+        Vector3 Vel= new Vector3 (0,Random.Range(0f,21f),Random.Range(0f,21f));
+        fJoint.targetAngularVelocity = Vel;
+        JointDrive drive = fJoint.slerpDrive;
+        drive.positionSpring = Random.Range(0,8);
+        drive.positionDamper = Random.Range(0,4);
+        fJoint.angularYZDrive= drive;
+        fJoint.connectedBody = currentTorso.GetComponent<Rigidbody>();
+
+        Debug.Log("added a limb to main torso right");
+        newLimb = Object.Instantiate(limbPrefab[0], transform.position, Quaternion.identity, currentTorso.transform);
+        ConfigurableJoint CJoint = newLimb.GetComponent<ConfigurableJoint>();
+        CJoint.anchor = new Vector3(-0.5f,0,0);
+        rand = Random.Range(0,2);
+        if (rand == 0)
+        {
+            CJoint.angularYMotion= ConfigurableJointMotion.Free;        
+        }
+        rand = Random.Range(0,2);
+        if (rand == 0)
+        {
+            CJoint.angularZMotion= ConfigurableJointMotion.Free;       
+        }
+        
+        Vel= new Vector3 (0,Random.Range(0f,21f),Random.Range(0f,21f));
+        CJoint.targetAngularVelocity = Vel;
+        drive = CJoint.slerpDrive;
+        drive.positionSpring = Random.Range(0,8);
+        drive.positionDamper = Random.Range(0,4);
+        CJoint.angularYZDrive= drive;
+        CJoint.connectedBody = currentTorso.GetComponent<Rigidbody>();
+
+        
+        /*/for(int i = 0; i < 2; i++){
             Debug.Log("added a limb to main torso");
-            newLimb = Object.Instantiate(limbPrefab, new Vector3(0f, 0f, 0f), Quaternion.Euler(
+            newLimb = Object.Instantiate(limbPrefab[randIndexLimb], transform.position, Quaternion.Euler(
                 Random.Range(0f, 360f),
                 Random.Range(0f, 360f),
                 Random.Range(0f, 360f)
             ), currentTorso.transform);
-            Vector3 limbScale = newLimb.transform.localScale;
-            newLimb.transform.localScale = new Vector3(
-                limbScale.x * Random.Range(0.1f, 0.8f),
-                limbScale.y * Random.Range(0.1f, 0.8f),
-                limbScale.z * Random.Range(0.1f, 0.8f));
-            randX = (int)Random.Range(0f, currentTorso.transform.localScale.x / 2 + newLimb.transform.localScale.x / 2);
-            randY = (int)Random.Range(newLimb.transform.localScale.y / 2,
-             currentTorso.transform.localScale.y / 2 + newLimb.transform.localScale.y / 2);
-            randZ = (int)Random.Range(0f, currentTorso.transform.localScale.z / 2 + newLimb.transform.localScale.z / 2);
-            newLimb.transform.position = new Vector3(randX, randY, randZ);
-            //On ajoute une fixedJoint pour rattacher les limbs au torso
-            FixedJoint fJoint = newLimb.GetComponent<FixedJoint>();
-            fJoint.connectedBody = currentTorso.GetComponent<Rigidbody>();
-        }
+        */
+
+            //On ajoute une ConfigurableJoint pour rattacher les limbs au torso
+            //ConfigurableJoint fJoint = newLimb.GetComponent<ConfigurableJoint>();
+            //fJoint.connectedBody = currentTorso.GetComponent<Rigidbody>();
+        //}
         /*for(int i = 0; i < adn.torsoSelfReflec; i++){
             newTorso = Object.Instantiate(torsoPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity, creature.transform);
             for(int y = 0; y < adn.limbNumb; y++){
---
+++ LifProjet2022/Assets/Scripts/CreatureSpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureSpawner : MonoBehaviour
+{
+
+    public GameObject Creature;
+
+    public int NbCreatures = 5;
+
+    public float posXSpread = 0, posYSpread = 1, posZSpread = 1;
+
+    public int TypeSpread = 1;
+
+    void Start()
+    {
+        for(int i=0 ; i < NbCreatures; i++){
+            SpreadCreatures(i);
+        }
+    }
+
+    void SpreadCreatures(float sep){
+        //X spread
+        if(TypeSpread == 1){
+            Vector3 randPos = new Vector3 (sep*2- (float)NbCreatures/2 + posXSpread , posYSpread, posZSpread)+ transform.position;
+            GameObject Spawn = Instantiate(Creature,randPos, Quaternion.identity);
+        } else if (TypeSpread == 2){
+            // Y Spread
+            Vector3 randPos = new Vector3 (posXSpread , sep*2 + posYSpread, posZSpread)+ transform.position;
+            GameObject Spawn = Instantiate(Creature,randPos, Quaternion.identity);
+        }else if (TypeSpread == 3) {
+            //Z Spread
+            Vector3 randPos = new Vector3 (posXSpread , posYSpread, sep*2- (float)NbCreatures/2 + posZSpread)+ transform.position;
+            GameObject Spawn = Instantiate(Creature,randPos, Quaternion.identity);
+        }else if (TypeSpread == 4){
+            // XZ Spread (Diagonale)
+            Vector3 randPos = new Vector3 (sep*2- (float)NbCreatures/2 + posXSpread , posYSpread, sep*2- (float)NbCreatures/2 + posZSpread)+ transform.position;
+            GameObject Spawn = Instantiate(Creature,randPos, Quaternion.identity);
+        }else {
+            //Licuadora
+            Vector3 randPos = new Vector3(posXSpread,posYSpread,posZSpread) +  transform.position;
+            GameObject Spawn = Instantiate(Creature,randPos, Quaternion.identity);
+        }
+    }
+
+}
