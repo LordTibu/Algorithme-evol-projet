02e6022b5c9b714ca1aa29641ea61f79ef85163b
Lenis <jhomilo47@gmail.com>
2022-12-12 06:18:16 +0100

    El infierno existe y se llama correjir el codigo de patino

---
+++ LifProjet2022/Assets/Scripts/CreatureController.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureController : MonoBehaviour
+{
+    public GameObject [] limbPrefab;
+    private GameObject creature, currentTorso, newTorso, currentLimb, newLimb, creaturesIN , newRefLimb;
+    private bool armL= false, armR= false, armT= false, armB= false, armRZ= false, armLZ = false;
+    public int limbNumb;
+    public float fitnessScore;
+    public Vector3 startPosition;
+    void Start()
+    {
+        currentTorso = this.gameObject;
+        startPosition = this.transform.position;
+    }
+
+    public void init(){
+        currentTorso = this.gameObject;
+        int rand = Random.Range(1, 7);
+        limbNumb = rand;
+        for(int i = 0; i< rand; i ++)
+        {
+            addRandLim();
+        }
+    }
+
+    // Update is called once per frame
+    void addArm(int n) {
+        ConfigurableJoint fJoint = null;
+        switch(n){ // 0 top, 1 Bottom, 2 left, 3 right, 4 rightz, 5 leftz
+            case 0 :armT = true; 
+                    newLimb = Object.Instantiate(limbPrefab[1], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>();
+                    fJoint.anchor = new Vector3(0, 0.2f - currentTorso.transform.localScale.y,0);
+                    break;
+            case 1 :armB = true;
+                    newLimb = Object.Instantiate(limbPrefab[1], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>();
+                    fJoint.anchor = new Vector3(0,currentTorso.transform.localScale.y - 0.2f,0);
+                    break;
+            case 2 :armL = true;
+                    newLimb = Object.Instantiate(limbPrefab[0], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>();
+                    fJoint.anchor = new Vector3(currentTorso.transform.localScale.x - 0.2f,0,0);
+                    break;
+            case 3 :armR = true;
+                    newLimb = Object.Instantiate(limbPrefab[0], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>(); 
+                    fJoint.anchor = new Vector3(0.2f - currentTorso.transform.localScale.x,0,0);
+                    break;
+            case 4 :armRZ = true;
+                    newLimb = Object.Instantiate(limbPrefab[2], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>(); 
+                    fJoint.anchor = new Vector3(0,0,0.2f -currentTorso.transform.localScale.z);
+                    break;
+            case 5 :armLZ = true;
+                    newLimb = Object.Instantiate(limbPrefab[2], transform.position, Quaternion.identity, currentTorso.transform);
+                    fJoint = newLimb.GetComponent<ConfigurableJoint>();  
+                    fJoint.anchor = new Vector3(0,0,currentTorso.transform.localScale.z - 0.2f);
+                    break;
+        }
+        int randL = Random.Range(0,2);
+        if (randL == 0)
+        {
+            fJoint.angularXMotion= ConfigurableJointMotion.Free;        
+        }
+        randL = Random.Range(0,2);
+        if (randL == 0)
+        {
+            fJoint.angularYMotion= ConfigurableJointMotion.Free;        
+        }
+
+        randL = Random.Range(0,2);
+        if (randL == 0)
+        {
+            fJoint.angularZMotion= ConfigurableJointMotion.Free;       
+        }
+
+        Vector3 Vel= new Vector3 (Random.Range(0f,21f),Random.Range(0f,21f),Random.Range(0f,21f));
+        fJoint.targetAngularVelocity = Vel;
+        JointDrive drive = fJoint.slerpDrive;
+        JointDrive drive2 = fJoint.slerpDrive;
+        drive.positionSpring = Random.Range(0,9);
+        drive.positionDamper = Random.Range(0,5);
+        drive2.positionSpring = Random.Range(0,9);
+        drive2.positionDamper = Random.Range(0,5);
+        fJoint.angularYZDrive= drive;
+        fJoint.angularXDrive= drive2;
+        fJoint.connectedBody = currentTorso.GetComponent<Rigidbody>();
+        
+        //Debug.Log("added a limb to main torso");
+    }
+
+    void addRandLim() {
+        int randL = Random.Range(0,6);
+        int randLL = Random.Range(0, 2);
+                    switch(randL){
+                        case 0: if(!armT){ // si il y a pas un bras sur le torse en top 
+                                    addArm(0);
+                                }else {
+                                    //addArmSelf(0);
+                                    addRandLim(); 
+                                    }
+                                    break;
+                                
+                        case 1: if(!armB){
+                                    addArm(1);
+                                }else {
+                                    //addArmSelf(1);
+                                    addRandLim () ;
+                                    }
+                                    break;
+                        case 2: if(!armL){
+                                    addArm(2);
+                                }else {
+                                    //addArmSelf(2);
+                                    addRandLim () ;
+                                    }
+                                    break;
+                        case 3: if(!armR){
+                                    addArm(3);
+                                }else {
+                                    //addArmSelf(3);
+                                    addRandLim () ;
+                                    } 
+                                    break;
+                        case 4: if(!armRZ){
+                                    addArm(4);
+                                }else {
+                                    //addArmSelf(4);
+                                    addRandLim () ;
+                                    } 
+                                    break;
+                        case 5: if(!armLZ){
+                                    addArm(5);
+                                }else {
+                                    //addArmSelf(5);
+                                    addRandLim () ;
+                                    } 
+                                    break;
+                    }
+    }
+
+    public void calculateFitness(){
+        startPosition[1] = 0;
+        Vector3 currentPos = this.transform.position;
+        currentPos[1] = 0;
+        fitnessScore = Vector3.Distance(currentPos, startPosition);
+    }
+
+    public void reSpawn(){
+        this.transform.position = startPosition;
+    }
+
+    public void crossover(GameObject parent1, GameObject parent2){
+        currentTorso = this.gameObject;
+        if(parent1 != null && parent2 != null){
+            GameObject nino;
+            int childs1 = parent1.transform.childCount;
+            int childs2 = parent2.transform.childCount;
+            int r = 0, pmin, pmax;
+            GameObject maxParent;
+            if(childs1 <= childs2){pmin = childs1; pmax = childs2; maxParent = parent2;}
+            else{pmin = childs2; pmax = childs1; maxParent = parent1;}
+            for(int i = 0; i < pmin; i++){
+                r = Random.Range(0, 2);
+                if(r == 0){
+                    nino = Instantiate(parent1.transform.GetChild(i).gameObject, this.transform);
+                    nino.GetComponent<ConfigurableJoint>().connectedBody = currentTorso.GetComponent<Rigidbody>();
+                }
+                else {
+                    nino = Instantiate(parent2.transform.GetChild(i).gameObject, this.transform);
+                    nino.GetComponent<ConfigurableJoint>().connectedBody = currentTorso.GetComponent<Rigidbody>();
+                }
+            }
+            for(int i = pmin; i < pmax; i++){
+                r = Random.Range(0, 2);
+                if(r == 0){
+                    nino = Instantiate(maxParent.transform.GetChild(i).gameObject, this.transform);
+                    nino.GetComponent<ConfigurableJoint>().connectedBody = currentTorso.GetComponent<Rigidbody>();
+                }
+            }
+        }
+    }
+
+    public void mutate(){
+
+    }
+
+    public void kill(){
+        Destroy(gameObject);
+    }
+}
---
+++ LifProjet2022/Assets/Scripts/CreatureManager.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreatureManager : MonoBehaviour
+{
+    public GameObject Eve = null;      // The creature used to create new creatures
+    //public GameObject Target;
+    //public CreatureController Champion;
+
+    public int NUMBER_OF_CREATURES = 48;
+    //Number of individuals per generation
+    public float PARENT_THRESHOLD = 0.5f;
+    //Percentage of the best individuals who can become parents
+    //Ex. 0.5f => The better half of the current generation can become parents
+    private List<CreatureController> creatures;
+
+    public static float MUTATION_RATE = 0.02f;
+    public static int SPACE_BETWEEN = 18;
+
+    public static float TIME_LIMIT = 300;
+    private float timeLeft = TIME_LIMIT;
+    public int generation = 0;
+    void Start()
+    {
+        if(PARENT_THRESHOLD > 1) PARENT_THRESHOLD = 0.5f;
+        if(!Eve){
+            Debug.Log("Not initialised a prefab");
+            this.enabled = false; //Disable script
+        }
+        creatures = new List<CreatureController>();
+        initCreatures();
+    }
+
+    void initCreatures(){
+        Vector3 position = Eve.transform.position;
+        int sqr = (int)Mathf.Sqrt((float) NUMBER_OF_CREATURES);
+        int x = 0, z = 0;
+        for(int i = 0; i < NUMBER_OF_CREATURES; i++){
+            position[0] = x * SPACE_BETWEEN;
+            position[2] = z * SPACE_BETWEEN;
+            GameObject currentIndividual = Instantiate(Eve, position, Quaternion.identity);
+            CreatureController copy = currentIndividual.GetComponent<CreatureController>();
+            //Naming the objects
+            copy.gameObject.name = "Individual: " + i;
+            creatures.Add(copy);
+            copy.init();
+            //updating the positions so they are organized in a (mostly) square grid
+            if(x < sqr) {x += 1;}
+            else{x = 0; z += 1;}
+        }
+    }
+
+    public void makeNewGeneration(){
+        generation += 1;
+        List<CreatureController>newCreatures = new List<CreatureController>();
+        GameObject randParent1, randParent2;
+        GameObject newChild;
+        CreatureController newCont;
+        int rand1, rand2;
+        Vector3 position = Vector3.zero;
+        calculateFitness();
+        
+        //On preserve le meilleur 10% des creatures
+        int ten = (int)(0.1 * NUMBER_OF_CREATURES);
+        int ninety = (int)(0.9 * NUMBER_OF_CREATURES);
+        int sqr = (int)Mathf.Sqrt((float) NUMBER_OF_CREATURES);
+        int x = 0, z = 0;
+        if(ten + ninety < NUMBER_OF_CREATURES) ten += 1;
+        for(int i = 0; i < ten; i++){
+            position[0] = x * SPACE_BETWEEN;
+            position[2] = z * SPACE_BETWEEN;
+            newChild = Instantiate(creatures[i].gameObject, position, Quaternion.identity);
+            newChild.name = creatures[i].name;
+            newCreatures.Add(newChild.GetComponent<CreatureController>());
+            if(x < sqr) {x += 1;}
+            else{x = 0; z += 1;}
+        }
+
+        for(int j = ten; j < NUMBER_OF_CREATURES; j++){
+            position[0] = x * SPACE_BETWEEN;
+            position[2] = z * SPACE_BETWEEN;
+            //On ajoute le reste des new creatures dans le tab.
+            //1. Select two random parent between the x% best individuals
+            rand1 = (int) Random.Range(0, PARENT_THRESHOLD * NUMBER_OF_CREATURES);
+            rand2 = (int) Random.Range(0, PARENT_THRESHOLD * NUMBER_OF_CREATURES);
+            while(rand1 == rand2) rand2 = (int) Random.Range(0, PARENT_THRESHOLD * NUMBER_OF_CREATURES);
+            randParent1 = creatures[rand1].gameObject;
+            randParent2 = creatures[rand2].gameObject;
+            if(randParent1 && randParent2){
+                newChild = Instantiate(Eve, position, Quaternion.identity);
+                //2. Crossover their genes
+                newCont = newChild.GetComponent<CreatureController>();
+                newCont.gameObject.name = "gen: " + generation + " Individual: " + j;
+                newCont.crossover(randParent1, randParent2);
+                //3. Mutate those genes a little bit
+                //newCont.mutate();
+                //4. Add new child individual to new tab
+                newCreatures.Add(newCont);
+            }
+            if(x < sqr) {x += 1;}
+            else{x = 0; z += 1;}
+        }
+        //Kill last generation
+        killCurrentGen(0);
+        creatures = new List<CreatureController>(newCreatures);
+    }
+
+    public void calculateFitness(){
+        for(int i = 0; i < NUMBER_OF_CREATURES; i++){
+            creatures[i].calculateFitness();
+        }
+        for(int i = 1; i < NUMBER_OF_CREATURES; i++)
+        {
+            for(int j = i; j > 0; j--)
+            {
+                if(creatures[j].fitnessScore > creatures[j-1].fitnessScore)
+                {
+                    CreatureController tmp = creatures[j];
+                    creatures[j] = creatures[j - 1];
+                    creatures[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public void killCurrentGen(int startPoint){
+        for(int i = startPoint; i < NUMBER_OF_CREATURES; i++){
+            creatures[i].kill();
+        }
+        creatures.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Restart the whole experiment 
+        if(Input.GetKey("r")) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if(Input.GetKey("f")) calculateFitness();
+        if(Input.GetKey("k")) killCurrentGen(0);
+
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0){
+            makeNewGeneration();
+            Debug.Log(TIME_LIMIT + " seconds have passed");
+            timeLeft = TIME_LIMIT;
+        }
+    }
+}
